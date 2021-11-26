19a286fd3006037fb6bed3f73920a78a08fa8578
Kevin <34973659+keketiger@users.noreply.github.com>
2021-11-26 06:09:43 +0100

    Transfert locale to shared

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,30 +6,14 @@ description 'This script add the fbi job, for esx framework.'
 website 'https://github.com/ESX-FRANCE/esx_fbi_job'
 version '0.0.4'
 
-server_scripts {
-	'@mysql-async/lib/MySQL.lua',
+shared_scripts {
 	'@es_extended/locale.lua',
 	'locales/en.lua',
 	'locales/fr.lua',
 	'locales/de.lua',
-	'config.lua',
-	'server/main.lua'
+	'config.lua'
 }
+server_scripts {'@mysql-async/lib/MySQL.lua','server/main.lua'}
+client_scripts {'client/main.lua','client/vehicle.lua'}
 
-client_scripts {
-	'@es_extended/locale.lua',
-	'locales/en.lua',
-	'locales/fr.lua',
-	'locales/de.lua',
-	'config.lua',
-	'client/main.lua',
-	'client/vehicle.lua'
-}
-
-dependencies {
-	'es_extended',
-	'esx_billing',
-	'esx_identity',
-	'esx_license',
-	'esx_service'
-}
+dependencies {'es_extended','esx_billing','esx_identity','esx_license','esx_service'}
